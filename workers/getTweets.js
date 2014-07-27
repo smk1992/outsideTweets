53ef19748cdaeae0a90c686f2e91d164d45a12e3
Salman <smku1992@gmail.com>
2014-07-26 19:17:43 -0700

    Add twit

--- workers/getTweets.js
+++ workers/getTweets.js
@@ -1,8 +1,22 @@
-var twit = require('twit');
+var Twit = require('twit');
+
+var consumer_key = require('./secrets.js').consumer_key;
+var consumer_secret = require('./secrets.js').consumer_secret;
+var access_token = require('./secrets.js').access_token_key;
+var access_token_secret = require('./secrets.js').access_token_secret;
+
+var twit = new Twit({
+  consumer_key: consumer_key,
+  consumer_secret: consumer_secret,
+  access_token: access_token,
+  access_token_secret: access_token_secret
+});
+
+
 
 // Getting Search Results of tweets about outsideLand
 function getTweets () {
-  
+
 
 };
 // Anaylyzing/Processing those tweets and and generating obj with tweets and score
