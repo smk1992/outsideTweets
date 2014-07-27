7e38f0216b146d1317048b5f80d8bf28386e0c18
Salman <smku1992@gmail.com>
2014-07-26 19:12:51 -0700

    Pseduo Worker

---
+++ workers/getTweets.js
@@ -0,0 +1,12 @@
+var twit = require('twit');
+
+// Getting Search Results of tweets about outsideLand
+function getTweets () {
+  
+
+};
+// Anaylyzing/Processing those tweets and and generating obj with tweets and score
+
+
+
+// Design A way to Store those tweets.
