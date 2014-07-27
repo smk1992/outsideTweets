dea34f6d92112be87791eaadab7022d5bbaedf94
Salman <smku1992@gmail.com>
2014-07-27 05:25:06 -0700

    Add replying back to tweets

--- helper.js
+++ helper.js
@@ -1,10 +1,19 @@
-var Artist = require('./artists.js');
+var Artist = require('./db/artists.js');
 
 var helper = {};
 
 // get artist
-helper.getArtistPerforms = function (filter) {
-  
+helper.getArtistPerforms = function (filter, callback) {
+  Artist.find(filter, function (err, results) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    if (results) {
+      callback(results); 
+    }
+  });
 };
 
 module.exports = helper;
