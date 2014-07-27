fdc216979ee1a3dbce87d0d638c4609af5482f1f
Salman <smku1992@gmail.com>
2014-07-26 19:26:10 -0700

    add stream to track outside land

--- workers/getTweets.js
+++ workers/getTweets.js
@@ -5,6 +5,7 @@ var consumer_secret = require('./secrets.js').consumer_secret;
 var access_token = require('./secrets.js').access_token_key;
 var access_token_secret = require('./secrets.js').access_token_secret;
 
+
 var twit = new Twit({
   consumer_key: consumer_key,
   consumer_secret: consumer_secret,
@@ -12,13 +13,13 @@ var twit = new Twit({
   access_token_secret: access_token_secret
 });
 
+// create stream to get searches 
+var stream = twit.stream('statuses/filter', { track : 'outside land' });
 
+stream.on('tweet', function (tweet) {
+  console.log('found tweet:', tweet);
+}
 
-// Getting Search Results of tweets about outsideLand
-function getTweets () {
-
-
-};
 // Anaylyzing/Processing those tweets and and generating obj with tweets and score
 
 
